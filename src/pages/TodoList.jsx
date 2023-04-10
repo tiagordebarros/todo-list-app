06f22937aa0777621a7a4f15580136a52b999610
Tiago Ribeiro de Barros <tiagordebarros@gmail.com>
2023-04-09 22:22:54 -0300

    fix: fix task removal by changing index to uuid

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,19 +1,20 @@
 import { useState } from "react";
 import AddTodoButton from "../components/AddTodoButton";
 import TodoItem from "../components/TodoItem";
+import { v4 as uuidv4 } from "uuid";
 
 function TodoList() {
   const [todoList, setTodoList] = useState([]);
 
   const getTaskList = (task) => {
     setTodoList((prevState) => {
-      return [...prevState, task];
+      return [...prevState, { id: uuidv4(), task }];
     });
   };
 
   const finishedTask = (id) => {
     setTodoList((prevState) => {
-      return prevState.filter((_, index) => index !== id);
+      return prevState.filter((todo) => todo.id !== id);
     });
   };
 
@@ -21,13 +22,8 @@ function TodoList() {
     <>
       <AddTodoButton taskList={getTaskList} />
       {todoList &&
-        todoList.map((todo, index) => (
-          <TodoItem
-            key={index}
-            id={index}
-            task={todo}
-            complete={finishedTask}
-          />
+        todoList.map(({ id, task }) => (
+          <TodoItem key={id} id={id} task={task} complete={finishedTask} />
         ))}
     </>
   );
